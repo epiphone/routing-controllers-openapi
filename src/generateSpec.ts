47ef27bd4ce9e3c514b5956cba23fcd79e78e49d
gstamac <1668205+gstamac@users.noreply.github.com>
2019-04-11 09:34:46 +0200

    handle explicit type to define param schema

--- src/generateSpec.ts
+++ src/generateSpec.ts
@@ -173,18 +173,18 @@ export function getRequestBody(route: IRoute): oa.RequestBodyObject | void {
   const bodyParamsSchema: oa.SchemaObject | null =
     bodyParamMetas.length > 0
       ? bodyParamMetas.reduce(
-          (acc: oa.SchemaObject, d) => ({
-            ...acc,
-            properties: {
-              ...acc.properties,
-              [d.name!]: getParamSchema(d)
-            },
-            required: isRequired(d, route)
-              ? [...(acc.required || []), d.name!]
-              : acc.required
-          }),
-          { properties: {}, required: [], type: 'object' }
-        )
+        (acc: oa.SchemaObject, d) => ({
+          ...acc,
+          properties: {
+            ...acc.properties,
+            [d.name!]: getParamSchema(d)
+          },
+          required: isRequired(d, route)
+            ? [...(acc.required || []), d.name!]
+            : acc.required
+        }),
+        { properties: {}, required: [], type: 'object' }
+      )
       : null
 
   const bodyMeta = route.params.find(d => d.type === 'body')
@@ -302,6 +302,15 @@ function getParamSchema(
   const { explicitType, index, object, method } = param
 
   const type = Reflect.getMetadata('design:paramtypes', object, method)[index]
+  if (_.isFunction(type) && type.name === 'Array') {
+    const items = explicitType
+      ? { $ref: '#/components/schemas/' + explicitType.name }
+      : { type: 'object' }
+    return { items, type: 'array' }
+  }
+  if (explicitType) {
+    return { $ref: '#/components/schemas/' + explicitType.name }
+  }
   if (_.isFunction(type)) {
     if (_.isString(type.prototype) || _.isSymbol(type.prototype)) {
       return { type: 'string' }
@@ -309,14 +318,10 @@ function getParamSchema(
       return { type: 'number' }
     } else if (_.isBoolean(type.prototype)) {
       return { type: 'boolean' }
-    } else if (type.name === 'Array') {
-      const items = explicitType
-        ? { $ref: '#/components/schemas/' + explicitType.name }
-        : { type: 'object' }
-      return { items, type: 'array' }
     } else if (type.name !== 'Object') {
       return { $ref: '#/components/schemas/' + type.name }
     }
   }
+
   return {}
 }
