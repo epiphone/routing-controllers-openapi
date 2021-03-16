e32668ba100e2466fbefbcc90f770b10e89491cc
miguel costa <miguelcostaparedes@gmail.com>
2021-03-16 22:59:34 +0000

    fix: prettier npm commands

--- __tests__/parameters.test.ts
+++ __tests__/parameters.test.ts
@@ -20,6 +20,7 @@ import {
 import { SchemaObject } from 'openapi3-ts'
 import { validationMetadatasToSchemas } from 'class-validator-jsonschema'
 import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator'
+// tslint:disable:no-implicit-dependencies no-submodule-imports
 import { defaultMetadataStorage } from 'class-transformer/cjs/storage'
 
 describe('parameters', () => {
