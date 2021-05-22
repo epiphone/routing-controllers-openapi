2eb9eb6f73ee1fe1fb50b95f97dca53804a89495
Roger Fedor <rdfedor@comchan.net>
2021-05-22 15:05:34 -0500

    Iss #81: Fix audit problems. Add importClassFromDirectories so controllers can be imported using string path.

--- src/parseMetadata.ts
+++ src/parseMetadata.ts
@@ -7,6 +7,7 @@ import { ActionMetadataArgs } from 'routing-controllers/types/metadata/args/Acti
 import { ControllerMetadataArgs } from 'routing-controllers/types/metadata/args/ControllerMetadataArgs'
 import { ParamMetadataArgs } from 'routing-controllers/types/metadata/args/ParamMetadataArgs'
 import { ResponseHandlerMetadataArgs } from 'routing-controllers/types/metadata/args/ResponseHandleMetadataArgs'
+import { importClassesFromDirectories } from './importClassesFromDirectories'
 
 /**
  * All the context for a single route.
@@ -26,6 +27,15 @@ export function parseRoutes(
   storage: MetadataArgsStorage,
   options: RoutingControllersOptions = {}
 ): IRoute[] {
+  // import all controllers and middlewares and error handlers (new way)
+  if (options && options.controllers && options.controllers.length) {
+    let controllerClasses: Function[];
+    controllerClasses = (options.controllers as any[]).filter(controller => controller instanceof Function);
+    const controllerDirs = (options.controllers as any[]).filter(controller => typeof controller === 'string');
+    controllerClasses.push(...importClassesFromDirectories(controllerDirs));
+    options.controllers = controllerClasses;
+  }
+  
   return storage.actions.map((action) => ({
     action,
     controller: storage.controllers.find(
