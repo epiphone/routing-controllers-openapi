569f32cb3fa486ebbaf54ed9210e664fee447298
Leonard Lin <leonard@linhome.ch>
2022-12-09 16:51:00 +0000

    updated deps for routing-controllers 0.10.0

--- src/generateSpec.ts
+++ src/generateSpec.ts
@@ -130,7 +130,7 @@ export function getPathParams(route: IRoute): oa.ParameterObject[] {
       const param: oa.ParameterObject = {
         in: 'path',
         name,
-        required: !token.optional,
+        required: !(token.modifier == '?'),
         schema: { type: 'string' },
       }
 
