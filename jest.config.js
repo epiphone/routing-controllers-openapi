d86895cc7acd782a1712f053914f32183adaa8c6
Aleksi Pekkala <aleksipekkala@gmail.com>
2025-03-02 11:20:28 +0900

    openapi3-ts v3.0.0 (#153)
    * openapi3-ts v3.0.0
    
    * Disable codecov

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: "(/__tests__/.*|\\.(test))\\.(ts|js)$",
+  testRegex: '(/__tests__/.*|\\.(test))\\.(ts|js)$',
   moduleDirectories: ['node_modules', 'src'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  roots: [
-    '<rootDir>/__tests__'
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/__tests__/fixtures/'
-  ],
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ]
-};
+  roots: ['<rootDir>/__tests__'],
+  testPathIgnorePatterns: ['/node_modules/', '/__tests__/fixtures/'],
+}
